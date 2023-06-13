6cedc5e013f8db0c4c573d15dd5f7bc4a5bc8fe9
Shehab Waleed <shehabwaleedd@gmail.com>
2023-06-13 22:00:39 +0300

    fixed some ratios errors when it is large screen in shop component

--- src/pages/shop/Shop.jsx
+++ src/pages/shop/Shop.jsx
@@ -4,29 +4,57 @@ import Search from './search/Search';
 import Products from './products/Products';
 import FilterTop from './filterTop/FilterTop';
 import FilterLeft from './filterLeft/FilterLeft';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 const Shop = () => {
-
     const [selectedProductId, setSelectedProductId] = useState(null);
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setWindowWidth(window.innerWidth);
+        };
+
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
+    const isLargeScreen = windowWidth >= 1732;
 
     return (
-        <section className="shop" style={{
-            height: selectedProductId ? "141vh" : "",
-            }}>
+        <section
+            className="shop"
+            style={{
+                height: selectedProductId ? '141vh' : '',
+            }}
+        >
             <div className="shop__container containered">
                 <FilterTop />
                 <Search />
                 <div className="shop__content">
                     <div className="shop__left">
-                        <FilterLeft selectedProductId={selectedProductId} setSelectedProductId={setSelectedProductId}/>
+                        <FilterLeft
+                            selectedProductId={selectedProductId}
+                            setSelectedProductId={setSelectedProductId}
+                        />
                     </div>
-                    <div className="shop__right" 
-                    style={{
-                        width: selectedProductId ? "" : "76vw",
+                    <div
+                        className="shop__right"
+                        style={{
+                            width: selectedProductId
+                                ? ''
+                                : isLargeScreen
+                                    ? '110vw'
+                                    : '76vw',
                         }}
                     >
-                        <Products selectedProductId={selectedProductId} setSelectedProductId={setSelectedProductId}/>
+                        <Products
+                            selectedProductId={selectedProductId}
+                            setSelectedProductId={setSelectedProductId}
+                        />
                     </div>
                 </div>
             </div>
