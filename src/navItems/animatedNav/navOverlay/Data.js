245a505a33df3c2d78a1a461ff20bdcc672002e6
Shehab Waleed <shehabwaleedd@gmail.com>
2023-06-14 15:51:11 +0300

    changed the overlay nav hover effect

--- src/navItems/animatedNav/navOverlay/Data.js
+++ src/navItems/animatedNav/navOverlay/Data.js
@@ -1,5 +1,8 @@
 import { useTranslation } from 'react-i18next';
-
+import img1 from "../../../assets/land5.jpg"
+import img2 from "../../../assets/lelu4.jpg"
+import img3 from "../../../assets/aboutus.jpg"
+import img4 from "../../../assets/lelu6.jpg"
 
 
 const Data = [
@@ -7,21 +10,25 @@ const Data = [
         id: 1, 
         title: "navbar__home",
         path: "/",
+        img: img1,
     },
     {
         id: 2,
         title: "FAQS",
         path: "/faqs",
+        img: img2,
     },
     {
         id: 3,
         title: "navbar__about",
-        path: "/about"
+        path: "/about",
+        img: img3,
     },
     {
         id: 4,
         title: "navbar__contact",
-        path: "/contact"
+        path: "/contact",
+        img: img4,
     },
 
 ]
--- src/navItems/animatedNav/navOverlay/NavOverlay.jsx
+++ src/navItems/animatedNav/navOverlay/NavOverlay.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/img-redundant-alt */
 import React, { useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import './NavOverlay.css'
@@ -20,21 +21,22 @@ const NavOverlay = (Props) => {
             transitionDelay: Props.navOpen ? "0s" : "0s"
         }}>
             <div className={Props.navOpen ? "nav-links spin" : "nav-links"}>
-                {Data.map(({ id, title, path }) => (
-                    <div className='menu-item-wrapper' style={{
-                        bottom: Props.navOpen ? "0" : "7.5rem",
-                        transitionDelay: Props.navOpen ? "0.7s" : "0s",
-                        opacity: Props.navOpen ? "1" : "0"
-                    }}>
-                        <div className="nav-item" key={id}>
+                {Data.map(({ id, title, path, img }) => (
+                    <div className='menu' style={{ bottom: Props.navOpen ? "0" : "7.5rem", transitionDelay: Props.navOpen ? "0.7s" : "0s", opacity: Props.navOpen ? "1" : "0" }}>
+                        <div className="menu__item" key={id}>
                             <Link to={path} onClick={() => Props.setNavOpen(!Props.navOpen)}>
-                                <h1 data-text={t(title)} style={{
-                                    top: Props.navOpen ? "0" : "7.5rem",
-                                    transitionDelay: Props.navOpen ? "0.7s" : "0s",
-                                    opacity: Props.navOpen ? "1" : "0"
-                                }}>
+                                <h1 className='menu__item-link' data-text={t(title)} style={{ top: Props.navOpen ? "0" : "7.5rem", transitionDelay: Props.navOpen ? "0.7s" : "0s", opacity: Props.navOpen ? "1" : "0" }}>
                                     {t(title)}
                                 </h1>
+                                <img className="menu__item-img" src={img} alt="Some image"/>
+                                <div className="marquee">
+                                    <div className="marquee__inner" aria-hidden="true">
+                                        <span>{t(title)}</span>
+                                        <span>{t(title)}</span>
+                                        <span>{t(title)}</span>
+                                        <span>{t(title)}</span>
+                                    </div>
+                                </div>
                             </Link>
                         </div>
                     </div>
@@ -47,13 +49,10 @@ const NavOverlay = (Props) => {
                 <div className="nav-social-media">
                     <ul>
                         <li>
-                            <a href="https://www.linkedin.com/in/shehabwaleedd/" target="_blank" rel="noreferrer">LI</a>
+                            <a href="https://www.linkedin.com/in/shehabwaleedd/" target="_blank" rel="noreferrer">FB</a>
                         </li>
                         <li>
-                            <a href="https://github.com/shehabwaleedd" target="_blank" rel="noreferrer">GH</a>
-                        </li>
-                        <li>
-                            <a href="https://www.instagram.com/shehabwaleedd/" target="_blank" rel="noreferrer">IG</a>
+                            <a href="https://www.instagram.com/lelu_storee/" target="_blank" rel="noreferrer">IG</a>
                         </li>
                     </ul>
                 </div>
