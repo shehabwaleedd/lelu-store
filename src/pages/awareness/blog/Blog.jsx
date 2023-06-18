8d09387d5d27515f6f5bf34c81c746ed35f914eb
Shehab Waleed <shehabwaleedd@gmail.com>
2023-06-18 12:19:45 +0400

    changed the logo font.

--- src/pages/awareness/blog/Blog.jsx
+++ src/pages/awareness/blog/Blog.jsx
@@ -18,10 +18,59 @@ const Blog = () => {
 
     const post = Data[id]
 
+    let screen = useRef(null);
+    let body = useRef(null);
+
+
+    const runAnimation = () => {
+        var tl = new TimelineMax();
+        tl.fromTo(
+            screen,
+            { width: "0%", left: "100%" },
+            {
+                duration: 0.5,
+                width: "100%",
+                left: "0%",
+                ease: Power3.easeInOut,
+            }
+        );
+        tl.fromTo(
+            screen,
+            { left: "0%" },
+            {
+                duration: 0.5,
+                left: "-100%",
+                ease: Power3.easeInOut,
+                delay: 0.1,
+            }
+        );
+        tl.set(screen, { left: "100%" });
+        TweenMax.to(body, 0.3, {
+            css: {
+                opacity: "1",
+                pointerEvents: "auto",
+                ease: Power4.easeInOut,
+            },
+        }).delay(1);
+    };
+
+
+    useEffect(() => {
+        runAnimation();
+    }, []);
+
+    useEffect(() => {
+        window.scrollTo({ top: 0});
+    }, []);
+
+
 
     return (
         <>
             <ScrollAnimation />
+            <div className="contact__load-container">
+                <div className="contact__load-screen" ref={(el) => (screen = el)}></div>
+            </div>
             <div className="blog__details">
                 <div className='blog__card_details'>
                     <div className='blog__image_container'>
@@ -34,9 +83,9 @@ const Blog = () => {
                                 <a href="/" className="post__hashtag-details">#{post.category}</a>
                             </div>
                         </div>
-                            <div className="blog__details_post-textanddate">
-                                <h3 className="post__title_name-details">{post.title}</h3>
-                            </div>
+                        <div className="blog__details_post-textanddate">
+                            <h3 className="post__title_name-details">{post.title}</h3>
+                        </div>
                         <div className="blog__details_post-text">
                             <p className="post__subtitle">{post.desc}</p>
                             <p className="post__subtitle">{post.content}</p>
